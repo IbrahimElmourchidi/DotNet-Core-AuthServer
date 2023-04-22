085c1e847df649a7477d7b071653c6295a653398
ibrahimElmourchidi <ibrahimelmourchidi@gmail.com>
2023-04-22 09:29:52 +0200

    initial state

---
+++ Basics/Program.cs
@@ -0,0 +1,23 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+    endpoints.MapDefaultControllerRoute()
+);
+
+app.Run();
